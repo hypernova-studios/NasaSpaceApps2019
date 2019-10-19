e31dd00c033ff34de97df60edb279a0c32b00193
Sam LaGrave <srl0011@uah.edu>
2019-10-19 15:02:38 -0500

    Card display and movability functionality

--- UnityProject/Assets/Scripts/Card.cs
+++ UnityProject/Assets/Scripts/Card.cs
@@ -5,11 +5,10 @@ using UnityEngine;
 [CreateAssetMenu(fileName="New Card", menuName="Card")]
 public class Card : ScriptableObject
 {
-    public new string name;
-    public string description;
-
     public Sprite artwork;
 
-    public int population;
+    public string text;
+
+    public string population;
 
 }
