162a6683021b1b974c0ffd1695fd33971d97590d
Phillip <iamaperson620@gmail.com>
2019-10-20 10:08:59 +0000

    working website

--- website/router.js
+++ website/router.js
@@ -1,8 +1,9 @@
 const express = require("express");
 const app = express();
 const port = 80;
+const ip = "10.0.0.10";
 
-app.get("/", (req, res) => res.sendFile(__dirname + "\\pages\\index.html"));
-app.use(express.static("pages"));
+app.get("/", (req, res) => res.sendFile(__dirname + "/pages/index.html"));
+app.use(express.static(__dirname + "/pages"));
 
-app.listen(port, () => console.log("Listening on port " + port));
+app.listen(port, ip, () => console.log("Listening on " + ip + ":" + port));
