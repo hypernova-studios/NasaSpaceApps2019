d5a02102c536ce090869bfef01546d419b0df2ec
Nathan Solomon <nbs0007@uah.edu>
2019-10-19 19:19:28 -0500

    Added start of game logic and player data model

---
+++ UnityProject/Assets/Scripts/Player.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Player : MonoBehaviour
+{
+    private int roundsWon;
+    private int roundsLost;
+    public Deck deck;
+
+
+    public Player()
+    {
+        roundsWon = 0;
+        roundsLost = 0;
+        deck = new Deck();
+    }
+
+    public void AddWin()
+    {
+        roundsWon++;
+    }
+
+    public void AddLoss()
+    {
+        roundsLost++;
+    }
+
+    public int GetRoundsWon()
+    {
+        return roundsWon;
+    }
+
+    public int GetRoundsLost()
+    {
+        return roundsLost;
+    }
+}
