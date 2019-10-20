05f4b64e1002cc34e69a955f15c013088b999fda
Sam LaGrave <srl0011@uah.edu>
2019-10-20 10:34:11 -0500

    Logic is working

--- UnityProject/Assets/Scripts/HandManager.cs
+++ UnityProject/Assets/Scripts/HandManager.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
+using System.Collections;
 
 public class HandManager : MonoBehaviour
 {
@@ -36,27 +37,26 @@ public class HandManager : MonoBehaviour
         }
         if(isTurn)
         {
-            var enemyCard = GameObject.Find("EnemyHandManager").GetComponentInChildren<EnemyHM>().playCard();
-            isTurn=false;
+            isTurn = false;
+            caller.GetComponent<CardDisplay>().enabled = false;
+            var enemyCard = GameObject.Find("EnemyHandManager").GetComponent<EnemyHM>().playCard();
             if(!GameObject.Find("GameRules").GetComponent<GameLogic>().EndRound(caller.GetComponent<CardDisplay>().card, enemyCard))
             {
                 Destroy(caller, 5);
                 Destroy(GameObject.Find("Player1PlayedCard").GetComponentInChildren<CardDisplay>().gameObject, 5);
             }
-            foreach(var i in x)
-            {
-                i.enabled = true;
-            }
         }
+        StartCoroutine(beginTurn());
     }
 
-    public void beginTurn()
+    IEnumerator beginTurn()
     {
+        yield return new WaitForSeconds(5);
         var x = GetComponentsInChildren<UIElementDragger>();
         foreach(var i in x)
         {
-            Debug.Log(i);
             i.enabled = true;
         }
+        isTurn = true;
     }
 }
